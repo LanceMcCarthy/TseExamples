7d3b6a68af364438f4c05966fcc21c0b182a0d4f
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-09-01 14:46:29 -0400

    Add sample data controller to Web API project (WideData)

---
+++ UploadingToWebApi/UploadingToWebApi.Web/Controllers/WideDataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace UploadingToWebApi.Web.Controllers
+{
+    public class WideDataController : ApiController
+    {
+        // GET: api/WideData
+        public Dictionary<string,string> Get(int startRow, int startColumn, int numberOfRows, int numberOfColumns)
+        {
+            var data = new Dictionary<string, string>();
+
+            for (var i = startColumn; i < numberOfColumns; i++)
+            {
+                for (var j = startRow; j < numberOfRows; j++)
+                {
+                    data[$"Column{j}"] = $"Row{j}";
+                }
+            }
+
+            return data;
+        }
+
+        // GET: api/WideData/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/WideData
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/WideData/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/WideData/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UploadingToWebApi/UploadingToWebApi.Web/Models/WideData/WideDataItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace QuickTypeUploadingToWebApi.Web.Models.WideData
+{
+    public enum WideDataItemValue { CellDataTest };
+
+    public class WideDataItem
+    {
+        public static Dictionary<string, WideDataItemValue>[] FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, WideDataItemValue>[]>(json, QuickTypeUploadingToWebApi.Web.Models.WideData.Converter.Settings);
+        }
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Dictionary<string, WideDataItemValue>[] self)
+        {
+            return JsonConvert.SerializeObject(self, QuickTypeUploadingToWebApi.Web.Models.WideData.Converter.Settings);
+        }
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                WideDataItemValueConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class WideDataItemValueConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(WideDataItemValue) || t == typeof(WideDataItemValue?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            if (value == "Cell data test")
+            {
+                return WideDataItemValue.CellDataTest;
+            }
+            throw new Exception("Cannot unmarshal type WideDataItemValue");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (WideDataItemValue)untypedValue;
+            if (value == WideDataItemValue.CellDataTest)
+            {
+                serializer.Serialize(writer, "Cell data test");
+                return;
+            }
+            throw new Exception("Cannot marshal type WideDataItemValue");
+        }
+
+        public static readonly WideDataItemValueConverter Singleton = new WideDataItemValueConverter();
+    }
+}
